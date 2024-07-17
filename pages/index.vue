f0e51df35d841220cbdf915cb9aacc07ea69444b
rdoVieira01 <ryanvo1911@gmail.com>
2024-07-16 21:39:18 -0300

    feat: changed home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,32 @@
 <template>
-  <div>
-    <el-button @click="$router.replace('quizlet')">
-      Quizlet Padrão
-    </el-button>
-    <el-button @click="$router.replace('custom')">
-      Customizado
-    </el-button>
+  <div class="main">
+    <el-card class="card__main">
+      <p class="mb-10 text-xl text-center">
+        Olá, seja bem vindo ao jogo <b>{{ gameName }}</b>
+      </p>
+      <div v-for="(line, idx) in description" :key="idx" class="mb-4 text-lg text-justify">
+        {{ line }}
+      </div>
+      <p class="mt-20 mb-10 text-xl text-center">
+        Clique em “Iniciar” para iniciar essa experiência.
+      </p>
+
+      <StepButton route="steps" text="Iniciar" />
+    </el-card>
   </div>
 </template>
 
-<style>
-* {
-  /* text-align: center; */
-}
+<script lang="ts" setup>
+import { useQuizStore } from '@/stores/quiz';
+
+const store = useQuizStore();
+await store.fetchQuiz();
+
+const gameName = computed(() => store.quiz?.name);
+const description = computed(() => store.quiz?.description?.split('\n'));
+
+</script>
+
+<style lang="scss">
+
 </style>
